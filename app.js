30e3785451762d1161a50ad32e7da908bdba3287
Kittinan Samakpong <joker.use.3@gmail.com>
2021-04-20 03:04:38 +0700

    home

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const  bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine',ejs);
+app.set('view engine','ejs');
 
 
 app.get('/',function(req,res){
-    res.render('Home,ejs');
-})
+    res.render('Home.ejs');
+});
+
+app.listen(3000,function(){
+    console.log('uCollection is started.');
+});

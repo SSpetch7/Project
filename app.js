23f185cc07079fd5e48e8e669a31db7ae9e91fbf
Kittinan Samakpong <joker.use.3@gmail.com>
2021-05-09 01:57:11 +0700

    css

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get('/',function(req,res){
     res.render('Home.ejs');
 });
 
+//Static File
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname+'public/css'))
+
 app.get('/collection',function(req,res){
     res.render('collection.ejs');
 });

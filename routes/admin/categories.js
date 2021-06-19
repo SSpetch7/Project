5f7f3667a0f389d9dac2bdb16533ce0a96cc2d1b
Kittinan Samakpong <joker.use.3@gmail.com>
2021-06-19 10:55:10 +0700

    add category 1

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -17,4 +17,41 @@ router.get('/add-category',function(req,res){
     res.render('admin/add.ejs',{ name: name});
 })
 
+router.post('/add-category',function(req,res){
+    req.checkBody('name','Name must have a value.').notEmpty();
+
+    var name = req.body.name;
+    var slug = req.replace(/\s+/g,'-').toLowerCase();
+    var errors = req.validationErros();
+
+    if(errors){
+        res.render('admin/add.ejs', {
+            errors: errors,
+            name : name
+        });
+    } else {
+        Category.findOne({slug: slug}, function(err,category){
+            if(category){
+                req.flash('error','Category name exists, choose another.');
+                res.render('admin/add.ejs',{name: name});
+            } else {
+                var category = new Category({
+                    name: name,
+                    slug: slug
+                });
+
+                category.save(function (err){
+                    if(err){
+                        console.log(err)
+                    } 
+
+                    req.flash('success','Category added!');
+                    res.redirect('/admin/categories');
+                    
+
+                })
+            }
+        })
+    }
+})
 module.exports = router;
